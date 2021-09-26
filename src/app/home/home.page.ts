8655f050bc37c6deb2c4d13275ee48483b360d14
IgorPVidal <igor.vidal@estudante.ufla.br>
2021-09-26 17:21:00 -0300

    Adiciona classificação do IMC
    Foi criado o método classificateIMC,() que é chamado pelo onCalculate(). O método recebe o valor do IMC e retorna a classificação, enquanto o método onCalculate() recebe a classificação e elabora a mensagem.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,7 +19,39 @@ export class HomePage {
 
   onCalculate() {
     const imc = this.weight / (this.height * this.height);
-    this.showMessage(`IMC = ${imc.toFixed(2)}`);
+    var mensagem = '';
+    var msg_classificacao = this.classificateIMC(imc);
+    if (msg_classificacao) {
+      mensagem = `IMC = ${imc.toFixed(2)}\n`;
+      msg_classificacao = `Classificação: ${msg_classificacao}`;
+    } else {
+      msg_classificacao = 'Erro ao calcular IMC.\nCertifique-se de que os valores de Peso e Altura são números maiores que zero (0).';
+    }
+    mensagem += `${msg_classificacao}`;
+    this.showMessage(mensagem);
+  }
+
+  classificateIMC(imc: number) {
+    var classificacao = '';
+    // Testes consideram a terceira casa decimal para maior coerência na impressão.
+    // Evita que um IMC = 18.495, que se torna um 18.5 na impressão, seja classificado como "Magreza",
+    // por exemplo.
+    if (imc < 18.495) { 
+      classificacao = 'Magreza';
+    } else if (imc < 24.995) {
+      classificacao = 'Normal';
+    } else if (imc < 29.995) {
+      classificacao = 'Sobrepeso';
+    } else if (imc <= 39.995) {
+      classificacao = 'Obesidade';
+    } else if (imc > 39.995) {
+      classificacao = 'Obesigade Grave';
+    } else {
+      // considerando a possibilidade desta função vir a ser utilizada em algum lugar 
+      // que não confere a validade do IMC.
+      classificacao = null;
+    }
+    return classificacao;
   }
 
   async showMessage(msg: string) {
